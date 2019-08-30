d0226811051394b6f67d366e5f1baa5772e59297
Anya <anya.vatamanyuk@gmail.com>
2019-08-30 14:49:48 +0300

    add home.scss, home.html

--- markup/src/js/pages/home.js
+++ markup/src/js/pages/home.js
@@ -1,3 +1,8 @@
-'use strict';
-
-let parJan = 3;
+$(function () {
+  $('#tabs-block_holder').tabs({
+    activate: function (event, ui) {
+      ui.oldTab.removeClass('active');
+      ui.newTab.addClass('active');
+    },
+  });
+});
